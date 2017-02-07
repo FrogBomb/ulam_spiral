efc960c542968f92907c99e149627ff5b9716bd0
FrogBomb <TLBlanchet@gmail.com>
2017-02-07 02:14:24 -0700

    Revert "Revert "Fixed recursion stack problem.""
    This reverts commit 189d58a1b21c9309405f70753a43a42d07e19394.

--- src/draw_spiral.rs
+++ src/draw_spiral.rs
@@ -4,7 +4,7 @@ use self::image::ImageBuffer;
 
 use std::fs::File;
 use std::path::PathBuf;
-
+use std::cmp;
 
 pub fn prime_filter_to_spiral_png(x_size: usize, from_vec: Vec<bool>, path: String){
     let imgbuf = make_spiral_buffer(x_size, from_vec);
@@ -24,7 +24,11 @@ fn point_on_spiral_to_array_index_end(spiral_size: usize, x: usize, y: usize)->u
         (x, y) if x == 0 => ss_minus_one*4 - y,
         (x, y) if y == spiral_size-1 => ss_minus_one*3 - x,
         (x, y) if x == spiral_size-1 => ss_minus_one + y,
-        (x, y) => ss_minus_one*4 + point_on_spiral_to_array_index_end(spiral_size-2, x-1, y-1),
+        (x, y) => {
+            let skip_loop_num = cmp::min(cmp::min(x, y), cmp::min(ss_minus_one-x, ss_minus_one-y));
+            (2*spiral_size - skip_loop_num)*skip_loop_num + point_on_spiral_to_array_index_end(
+                    spiral_size-2*skip_loop_num, x-skip_loop_num, y-skip_loop_num)
+        },
     }
 
 }
