f5a21319315533f13e046c17c54d299b9c2dadd0
Tom Blanchet <TLBlanchet@gmail.com>
2017-02-07 22:58:53 -0700

    Even better primes (#2)
    * Restructured primes file
    
    added a new old_prime_filter, replaced old with slow_prime_filter, and
    added a test
    
    * forgot some stuff
    
    whoops
    
    * Initial bug fixes
    
    fixed some default values, incorrect parameter usage, and a line I was
    using to test some behavior
    
    * Relabel, no improvement to algorithm
    
    For now, I'll just pull the test.

--- src/primes.rs
+++ src/primes.rs
@@ -2,7 +2,7 @@ use std;
 pub fn prime_filter(iter_size: usize) -> std::vec::Vec<bool>{
     //Sieve of Atkin
     if iter_size < 100 {
-        old_prime_filter(iter_size)
+        slow_prime_filter(iter_size)
     }else{
         let mut prime_filter = vec![false; iter_size];
         prime_filter[2] = true;
@@ -83,27 +83,16 @@ pub fn prime_filter(iter_size: usize) -> std::vec::Vec<bool>{
     }
 }
 
-fn old_prime_filter(iter_size: usize) -> std::vec::Vec<bool>{
-    if iter_size < 5 {
-         let mut ret = vec![false, false, true, true];
-         ret.truncate(iter_size);
-         return ret
-     }
-    let mut prime_filter = vec![true; iter_size];
-    prime_filter[0] = false;
-    prime_filter[1] = false;
-    let mut cur_num = 2;
-    'outer: loop{
-        for i in (cur_num+1)..iter_size{
-            if 0 == i%cur_num { prime_filter[i] = false; }
-        }
-        cur_num += 1;
-        while !prime_filter[cur_num]{
-            if cur_num*cur_num > iter_size {
-                break 'outer
-            }
-            cur_num += 1;
-        }
+pub fn old_prime_filter(iter_size: usize) -> std::vec::Vec<bool>{
+    slow_prime_filter(iter_size)
+    
+//not idiomatic, but I'm doing it anyway.
+#[test]
+fn test_prime_filter(){
+    let n = 1234567;
+    let new_pf = prime_filter(n);
+    let old_pf = old_prime_filter(n);
+    for i in 0..n{
+        assert_eq!(new_pf[i], old_pf[i], "Failed on number {}", i)
     }
-    prime_filter
 }
