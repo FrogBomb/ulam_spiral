ff441cfcc54bc52c4167df97969c0cca22a1b19e
FrogBomb <TLBlanchet@gmail.com>
2017-02-07 23:31:12 -0700

    Forgot names
    WHOOPS!

--- src/primes.rs
+++ src/primes.rs
@@ -83,9 +83,32 @@ pub fn prime_filter(iter_size: usize) -> std::vec::Vec<bool>{
     }
 }
 
-pub fn old_prime_filter(iter_size: usize) -> std::vec::Vec<bool>{
+fn old_prime_filter(iter_size: usize) -> std::vec::Vec<bool>{
     slow_prime_filter(iter_size)
-    
+
+fn slow_prime_filter(iter_size: usize) -> std::vec::Vec<bool>{
+    if iter_size < 5 {
+         let mut ret = vec![false, false, true, true];
+         ret.truncate(iter_size);
+         return ret
+     }
+    let mut prime_filter = vec![true; iter_size];
+    prime_filter[0] = false;
+    prime_filter[1] = false;
+    let mut cur_num = 2;
+    'outer: loop{
+        for i in (cur_num+1)..iter_size{
+            if 0 == i%cur_num { prime_filter[i] = false; }
+        }
+        cur_num += 1;
+        while !prime_filter[cur_num]{
+            if cur_num*cur_num > iter_size {
+                break 'outer
+            }
+            cur_num += 1;
+        }
+    }
+}
 //not idiomatic, but I'm doing it anyway.
 #[test]
 fn test_prime_filter(){
